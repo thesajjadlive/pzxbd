a5f41236afa4230f4f1a3a3b25652a6349411f6a
Sajjad Hossain <thesajjadlive@gmail.com>
2020-03-07 16:28:46 +0600

    modified

--- resources/views/layouts/front/_head.blade.php
+++ resources/views/layouts/front/_head.blade.php
@@ -9,7 +9,8 @@
 <meta name="author" content="SW-THEMES">
 
 <!-- Favicon -->
-<link rel="stylesheet" href="{{ asset('assets/frontend/css/font-awesome.css') }}">
+{{--<link rel="stylesheet" href="{{ asset('assets/frontend/css/font-awesome.css') }}">--}}
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/frontend/images/icons/favicon.ico') }}">
 
 <!-- Bootstrap 4.0-->
